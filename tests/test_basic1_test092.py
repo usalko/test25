671c07a3f05bebac72a8b6fa7d1870534f6bf68d
Vanya Usalko <dk4aol@gmail.com>
2024-11-30 21:14:19 +0300

    # There is the TODO for test092.

--- tests/test_basic1_test092.py
+++ tests/test_basic1_test092.py
@@ -8,6 +8,26 @@ class TestsBasic1Test092(TestCase):
     '''
     Write a Python program to define a string containing special characters in various forms.
     '''
+    # TODO: consider how to test that:
+    # Print a string containing special characters without escaping.
+    # print()
+    # print("\#{'}${\"}@/")
+
+    # # Print a string containing special characters with escaped single quotes.
+    # print("\#{'}${\"}@/")
+
+    # # Print a raw string using triple-quotes with special characters.
+    # print(r"""\#{'}${"}@/""")
+
+    # # Print a string containing special characters without escaping.
+    # print('\#{\'}${"}@/')
+
+    # # Print a string containing special characters with escaped single quotes.
+    # print('\#{'"'"'}${"}@/')
+
+    # # Print a raw string using triple-quotes with special characters.
+    # print(r'''\#{'}${"}@/''')
+    # print()
 
     def test_case1(self):
         for special_character in punctuation:
